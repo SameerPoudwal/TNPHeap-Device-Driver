85453c08e201b338dd5eb601dd75bca4cc3bb357
ssgodbol <ssgodbol@ncsu.edu>
2017-10-30 22:47:59 -0400

    debug errors

--- library/tnpheap.c
+++ library/tnpheap.c
@@ -16,14 +16,14 @@ struct bufferNode {
     __u64 size;
     void* addr;
     __u64 version;
-    bufferNode* next;
+    struct bufferNode* next;
 };
 struct bufferNode *buffer_head;
 
 __u64 tnpheap_get_version(int npheap_dev, int tnpheap_dev, __u64 offset)
 {
     struct tnpheap_cmd cmd;
-    cmd.offset = offset.getpagesize();
+    cmd.offset = offset;
     return tnpheap_ioctl(tnpheap_dev,TNPHEAP_IOCTL_GET_VERSION,&cmd);
 }
 
@@ -69,17 +69,17 @@ __u64 tnpheap_start_tx(int npheap_dev, int tnpheap_dev)
 
 int tnpheap_commit(int npheap_dev, int tnpheap_dev)
 {
-    struct cmd cmd;
-    struct node *temp = buffer_head;
+    struct tnpheap_cmd cmd;
+    struct bufferNode *temp = buffer_head;
     while(temp->next!=NULL){
         cmd.offset = temp->objectId;
         __u64 currentVersion = ioctl(tnpheap_dev,TNPHEAP_IOCTL_GET_VERSION,&cmd);
         if(currentVersion != temp->version){
-            return -1
+            return -1;
         }
         temp = temp->next;
     }
-    temp = head;
+    temp = buffer_head;
     while(temp!=NULL){
         if(ioctl(tnpheap_dev,TNPHEAP_IOCTL_COMMIT)==(__u64)0){
             void *mapped_data = npheap_alloc(npheap_dev,temp->objectId,temp->size);
