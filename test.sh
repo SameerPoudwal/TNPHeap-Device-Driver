46526c70de116b53630227ad8ac2088ebe73aa26
ssgodbol <ssgodbol@ncsu.edu>
2017-10-31 17:36:27 -0400

    steps

--- test.sh
+++ test.sh
@@ -1,3 +1,28 @@
+sudo insmod /NPHeap/npheap.ko
+sudo chmod 777 /dev/npheap
+cd kernel_module
+sudo make clean
+make
+sudo make install
+cd ..
+cd library
+sudo make clean
+make
+sudo make install
+cd ..
+cd benchmark
+sudo make clean
+sudo make benchmark
+sudo make validate
+cd ..
+sudo insmod kernel_module/tnpheap.ko
+sudo chmod 777 /dev/tnpheap
+./benchmark/benchmark 4 8192 1
+
+
+
+
+
 # change this to where your npheap is.
 sudo insmod ../NPHeap/kernel_module/npheap.ko
 sudo chmod 777 /dev/npheap
